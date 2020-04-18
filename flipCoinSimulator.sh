b732002fdd2197aa3ee69e5c19271aef1d10e52d
SirsikarAkshay <akshaysirsikar@gmail.com>
2020-04-18 16:20:13 +0530

    Winner is found

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-for(( i=0; i<10; i++ ))
-do
+heads=0
+tails=0
 
+while [ $heads -lt 21 ] || [ $tails -lt 21 ]
+do
 dec=$(($RANDOM%2+1))
 
 if(($dec==1))
 then
-	echo "Heads"
+	heads=$(($heads+1))
 else
-	echo "Tails"
+	tails=$(($tails+1))
+fi
+done
+
+if(($heads==21))
+then
+	echo "heads is the winner"
+else
+	echo "Tails is the winner"
 fi
 
-done
