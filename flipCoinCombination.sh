ba8835605c8cf746e703696345d30f9637fffe24
SirsikarAkshay <akshaysirsikar@gmail.com>
2020-04-19 11:54:51 +0530

    Stored singlet dictionary and percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,35 @@
 #!/bin/bash 
 
-dec=$(($RANDOM%2+1))
+declare -A singletDic
+heads=0
+tails=0
 
-if(($dec==1))
-then
-	echo "heads"
-else
-	echo "tails"
-fi
+headArray=()
+tailArray=()
+
+for (( i=0; i<10; i++ ))
+do
+
+	dec=$(($RANDOM%2+1))
+
+	if(($dec==1))
+	then
+		heads=$(($heads+1))
+		headArray+=('heads')
+	else
+		tails=$(($tails+1))
+		tailArray+=('tails')
+	fi
+done
+
+singletDic[head]=headArray
+singletDic[tail]=tailArray
+headsPercent=$((100*$heads/10))
+tailsPercent=$((100*$tails/10))
+
+echo "${headArray[@]}"
+echo "${tailArray[@]}"
+
+echo "heads percentage is $headsPercent"
+echo "tails percentage is $tailsPercent"
 
