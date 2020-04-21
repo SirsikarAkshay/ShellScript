140f7b4e2e38e6e88c9d35a0d32d0586c8d74dbe
SirsikarAkshay <akshaysirsikar@gmail.com>
2020-04-21 12:37:17 +0530

    Sorting and winning combination updated

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+#singlet combination sort
 
 declare -A singletDic
 heads=0
@@ -42,6 +43,79 @@ then
 else
 	echo "tied"
 fi
+
+#Doublet combination sort
+declare -A doubletDic
+headHead=0
+headTail=0
+tailHead=0
+tailTail=0
+
+headHeadArray=()
+headTailArray=()
+tailHeadArray=()
+tailTailArray=()
+
+for (( i=0; i<10; i++ ))
+do
+
+        dec1=$(($RANDOM%4))
+
+        if(($dec1==0))
+        then
+                headHead=$(($headHead+1))
+                headHeadArray+=('H,H')
+        elif(($dec1==1))
+        then
+                headTail=$(($headTail+1))
+                headTailArray+=('H,T')
+        elif(($dec1==2))
+        then
+                tailHead=$(($tailHead+1))
+                tailHeadArray+=('T,H')
+        else
+                tailTail=$(($tailTail+1))
+                tailTailArray+=('T,T')
+        fi
+done
+
+doubletDic[headHeads]=headHeadArray
+doubletDic[headTails]=headTailArray 
+doubletDic[tailHeads]=tailHeadArray                                                                                     
+doubletDic[tailTails]=tailTailArray
+
+HHPercent=$((100*$headHead/10))
+HTPercent=$((100*$headTail/10))
+THPercent=$((100*$tailHead/10))
+TTPercent=$((100*$tailTail/10))
+
+echo "${headHeadArray[@]}"
+echo "${headTailArray[@]}"
+echo "${tailHeadArray[@]}"
+echo "${tailTailArray[@]}"
+
+echo "heads,heads percentage is $HHPercent" 
+echo "heads,tails percentage is $HTPercent"
+echo "tails,heads percentage is $THPercent"
+echo "tails,tails percentage is $TTPercent"
+
+if(($HHPercent>$HTPercent)) && (($HHPercent>$THPercent)) && (($HHPercent>$TTPercent))
+then
+        echo "Heads,Heads wins!!!"
+elif(($HTPercent>$HHPercent)) && (($HTPercent>$THPercent)) && (($HTPercent>$TTPercent))
+then
+        echo "Heads,tails wins!!!"
+elif(($THPercent>$HHPercent)) && (($THPercent>$HTPercent)) && (($THPercent>$TTPercent))
+then
+        echo "Tails, heads wins!!!"
+elif(($TTPercent>$HHPercent)) && (($TTPercent>$HTPercent)) && (($TTPercent>$THPercent))
+then
+        echo "tails, tails wins!!!"
+else
+        echo "Tied between combinations"
+fi
+
+#triplet sorting
 
 declare -A tripletDic
 headHeadHead=0
@@ -147,5 +221,31 @@ done | sort -nr | head -n1`
 
 echo "Highest occurence is $max"
 
-
+if(($HHHPercent>$HHTPercent)) && (($HHHPercent>$HTHPercent))  && (($HHHPercent>$HTTPercent)) && (($HHHPercent>$THHPercent)) && (($HHHPercent>$THTPercent))  && (($HHHPercent>$TTHPercent)) && (($HHHPercent>$TTTPercent)) 
+then
+	echo "HHH Wins"
+elif(($HHTPercent>$HHHPercent)) && (($HHTPercent>$HTHPercent)) && (($HHTPercent>$HTTPercent)) && (($HHTPercent>$THHPercent)) && (($HHHPercent>$THTPercent)) && (($HHTPercent>$TTHPercent)) && (($HHTPercent>$TTTPercent))
+then
+	echo "HHT Wins"
+elif(($HTHPercent>$HHHPercent)) && (($HTHPercent>$HHTPercent)) && (($HTHPercent>$HTTPercent)) && (($HTHPercent>$THHPercent)) && (($HTHPercent>$THTPercent)) && (($HTHPercent>$THTPercent)) && (($HHHPercent>$TTTPercent))
+then
+	echo "HTH Wins"
+elif(($HTTPercent>$HHHPercent)) && (($HTTPercent>$HHTPercent)) && (($HTTPercent>$HTHPercent)) && (($HTTPercent>$THHPercent)) && (($HHHPercent>$THTPercent)) && (($HHHPercent>$TTHPercent)) && (($HHHPercent>$TTTPercent)) 
+then
+	echo "HTT Wins"
+elif(($THHPercent>$HHHPercent)) && (($THHPercent>$HHTPercent)) && (($THHPercent>$HTHPercent)) && (($THHPercent>$HTTPercent)) && (($THHPercent>$THTPercent)) && (($THHPercent>$TTHPercent)) && (($THHPercent>$TTTPercent)) 
+then
+	echo "THH Wins"
+elif(($THTPercent>$HHHPercent)) && (($THTPercent>$HHTPercent)) && (($THTPercent>$HTHPercent)) && (($THTPercent>$HTTPercent)) && (($THTPercent>$THHPercent)) && (($THTPercent>$TTHPercent)) && (($THTPercent>$TTTPercent))
+then
+	echo "THT Wins"
+elif(($TTHPercent>$HHHPercent)) && (($TTHPercent>$HHTPercent)) && (($TTHPercent>$HTHPercent)) && (($TTHPercent>$HTTPercent)) && (($TTHPercent>$THHPercent)) && (($TTHPercent>$THTPercent)) && (($TTHPercent>$TTTPercent))
+then
+	echo "TTH Wins"
+elif(($TTTPercent>$HHHPercent)) && (($TTTPercent>$HHTPercent)) && (($TTTPercent>$HTHPercent)) && (($TTTPercent>$HTTPercent)) && (($TTTPercent>$THHPercent)) && (($TTTPercent>$THTPercent)) && (($TTTPercent>$TTHPercent))
+then 
+	echo "TTT Wins"
+else
+	echo "Tied between combi"
+fi
 
